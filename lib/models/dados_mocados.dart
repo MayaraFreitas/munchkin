182c49486928f7ffe0bcb4b57ef1e6b7a5f7aed8
RodrigoDisselli <disselli.rodrigo@gmail.com>
2019-10-17 01:03:16 -0300

    criação da primeira página (precisa de melhoria na validação do campo que sofrera adição ou subtração no método _scoreActionListItem() ), o tema foi modificado para dark e a rotação da tela foi bloqueada para evitar a quebra do layout, as funções de adição e subtração de pontos ja funcionam, junto a validação dos dados

---
+++ lib/models/dados_mocados.dart
@@ -0,0 +1,46 @@
+var jogadores = ["jogador 1", "jogador 2", "jogador 3", "jogador 4", "jogador 5"];
+
+class Jogador {
+  var nome;
+  var sexo;
+  int level;
+  int forca;
+  int posicao;
+  
+  Jogador(this.nome, this.sexo, this.level, this.forca, this.posicao);
+
+  int getScoreTotal(Jogador jogador){
+    int scoreTotal = jogador.level + jogador.forca;
+
+    return scoreTotal;
+  }
+
+  String setSexo(Jogador jogador) {
+    if (jogador.sexo == "M"){
+      return jogador.sexo = "F";
+    }else {
+      return jogador.sexo = "M";
+    }
+  }
+
+  String getPosicao(Jogador jogador) {
+    return jogador.posicao.toString();
+  }
+
+  int addLevel(Jogador jogador) {
+    return jogador.level += 1;
+  }
+
+  int addForca(Jogador jogador) {
+    return jogador.forca += 1;
+  }
+
+  int removeLevel(Jogador jogador) {
+    return jogador.level -= 1;
+  }
+
+  int removeForca(Jogador jogador) {
+    return jogador.forca -= 1;
+  }
+
+}
---
+++ lib/views/score_page.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:munchkin/models/dados_mocados.dart';
+
+
+class ScorePage extends StatefulWidget {
+  @override
+  _ScorePageState createState() => _ScorePageState();
+}
+
+Jogador jogador = new Jogador("Jogador 1", "M", 1, 0, 3);
+
+class _ScorePageState extends State<ScorePage> {
+
+ void _showDialog(
+  {String title, String message, Function confirm, Function cancel}){
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title ?? ""),
+          content: Text(message ?? ""),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("CANCELAR", style: TextStyle(color: Colors.grey[400])),
+              onPressed: (){
+                Navigator.of(context).pop();
+                if(cancel != null) cancel();
+              },
+            ),
+            FlatButton(
+              child: Text("CONFIRMAR", style: TextStyle(color: Colors.greenAccent[400])),
+              onPressed: (){
+                Navigator.of(context).pop();
+                if(confirm != null) confirm();
+              },
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+  Widget _scorePanel() {
+    return Container(
+      child: Container(
+        margin: const EdgeInsets.only(left: 0.0, right: 0.0, top: 0.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(jogador.getScoreTotal(jogador).toString(), style: TextStyle(fontSize: 90.0, letterSpacing: 12.0)),
+                Text("Força total"),
+                Container(
+                    margin: EdgeInsets.only(top: 20.0),
+                    child: FlatButton(
+                      onPressed: () {
+                        /*DEVE IR PARA PÁGINA DA MESA*/
+                      },
+                      child: IconButton(
+                      icon: Icon(jogador.sexo == "F" ? Icons.person : Icons.person_outline, color: Colors.grey[50]),
+                      onPressed: () {
+                        setState(() {
+                          jogador.setSexo(jogador); 
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.only(top: 30.0),
+                    child: FlatButton(
+                      onPressed: () {
+                        /*DEVE IR PARA PÁGINA DA MESA*/
+                      },
+                      child: Text(jogador.getPosicao(jogador) + "º POSIÇÃO",)
+                  ),
+                )
+              ],    
+            )
+        ),
+        color: Colors.redAccent[400],
+        height: 300.0,
+      ),
+    );
+  }
+
+
+  Widget _scorePage(){
+    return Column(
+      children: <Widget>[
+        _scorePanel(),
+        _scoreActionList()
+      ],
+    );
+  }
+
+  Widget _scoreActionListItem(int value, String label, String nomeCampo){
+    return Padding(
+      padding: EdgeInsets.only(top: 40.0, bottom: 40.0, left: 25.0, right: 25.0),
+      child:Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: Text(value.toString(), style: TextStyle(fontSize: 36.0))       
+              ),
+              Text(label, style: TextStyle(fontSize: 20.0, color: Colors.grey[400])),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 50.0),
+                child: IconButton(
+                  icon: Icon(Icons.remove),
+                  onPressed: () {
+                    setState(() {
+                      if(jogador.level > 1){
+                        nomeCampo == "level" ? jogador.level = jogador.removeLevel(jogador) : jogador.forca = jogador.removeForca(jogador);
+                      }else {
+                        if((nomeCampo == "forca") && (jogador.forca > 0)){
+                          jogador.forca = jogador.removeForca(jogador);
+                        }
+                      }
+                    });
+                  },
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.add, color: Colors.greenAccent[400]),
+                onPressed: () {
+                  setState(() {
+                    if(jogador.level < 10){
+                      nomeCampo == "level" ? jogador.level = jogador.addLevel(jogador) : jogador.forca = jogador.addForca(jogador);
+                    }
+                    if(jogador.level >= 10){
+                      _showDialog(
+                        title: 'Fim de jogo',
+                        message: "Você atingiu o level 10",
+                        confirm: () {
+                          setState(() {
+                          });
+                        },
+                        cancel: () {
+                          setState(() {
+                            jogador.level = jogador.removeLevel(jogador);
+                          });
+                        }
+                      );
+                    }
+                  });
+                },
+              )
+            ],
+          )
+        ]
+      )
+    );
+  }
+
+  Widget _scoreActionList(){
+    return Column(
+      children: <Widget>[
+        _scoreActionListItem(jogador.level, "Level", "level"),
+        Divider(),
+        _scoreActionListItem(jogador.forca, "Força", "forca")
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: Text("Munchkin", style: TextStyle(color: Colors.grey[50])),
+        actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.casino , color: Colors.grey[50]),
+          onPressed: () {
+            // JOGAR DADOS
+          },
+        )
+      ],
+      ),
+      body: _scorePage(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}

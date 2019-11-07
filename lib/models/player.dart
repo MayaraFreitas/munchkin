9821d9d12fe4509995bd9879617664a5e4125167
MayaraFreitas <mah.alvesfreitas@gmail.com>
2019-11-07 20:21:39 -0300

    correção de parse

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -23,8 +23,9 @@ class Player{
   factory Player.fromJson(String str) => Player.fromMap(json.decode(str));
 
   factory Player.fromMap(Map<String,dynamic> json) => Player(
-    forca: json["forca"] == "" ? 0: int.parse(json["forca"]),
-    level: json["level"] == "" ? 0: int.parse(json["level"]),
+    
+    forca: json["forca"],
+    level: json["level"],
     name: json["name"],
     sexo: json["sexo"]
   );
